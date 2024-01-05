796a3a9b42ea436841a522cc0e0993b4b71b0e2a
chandan <chandan7979ck@gmail.com>
2024-01-05 17:00:32 +0530

    done with UI

---
+++ src/components/Cards.jsx
@@ -0,0 +1,172 @@
+import React from 'react';
+import './Cards.css';
+
+const Cards = () => {
+  return (
+    <main class="main_container">
+
+      {/* <!-- Card --> */}
+      <div class="card_container">
+        <a href="#" class="card_image_container">
+          <img
+            src="https://source.unsplash.com/random/600x400?girls"
+            alt="card image"
+            class="card_image"
+            loading="lazy"
+          />
+        </a>
+        <div class="card_title_container">
+          <h2 class="card_name">Chandan Kushwaha</h2>
+          <div className="card_details">
+            <p><i class="ri-mail-fill"></i>chandan@google.com</p>
+            <p><i class="ri-phone-fill"></i>+91-7050936557</p>
+            <p><i class="ri-global-fill"></i>https://chandanck22.vercel.app/</p>
+          </div>
+        </div>
+
+        <div class="card_footer_container">
+          <div class="card_tag_container">
+            <a href=""><i class="ri-service-fill"></i></a>
+          </div>
+          <div class="card_tag_container">
+            <a href="#"><i class="ri-edit-fill"></i></a>
+          </div>
+          <div class="card_tag_container">
+            <a href="#"><i class="ri-delete-bin-7-fill"></i></a>
+          </div>
+        </div>
+      </div>
+
+            {/* <!-- Card --> */}
+            <div class="card_container">
+        <a href="#" class="card_image_container">
+          <img
+            src="https://source.unsplash.com/random/600x400?girls"
+            alt="card image"
+            class="card_image"
+            loading="lazy"
+          />
+        </a>
+        <div class="card_title_container">
+          <h2 class="card_name">Chandan Kushwaha</h2>
+          <div className="card_details">
+            <p><i class="ri-mail-fill"></i>chandan@google.com</p>
+            <p><i class="ri-phone-fill"></i>+91-7050936557</p>
+            <p><i class="ri-global-fill"></i>https://chandanck22.vercel.app/</p>
+          </div>
+        </div>
+
+        <div class="card_footer_container">
+          <div class="card_tag_container">
+            <a href=""><i class="ri-service-fill"></i></a>
+          </div>
+          <div class="card_tag_container">
+            <a href="#"><i class="ri-edit-fill"></i></a>
+          </div>
+          <div class="card_tag_container">
+            <a href="#"><i class="ri-delete-bin-7-fill"></i></a>
+          </div>
+        </div>
+      </div>
+
+            {/* <!-- Card --> */}
+            <div class="card_container">
+        <a href="#" class="card_image_container">
+          <img
+            src="https://source.unsplash.com/random/600x400?girls"
+            alt="card image"
+            class="card_image"
+            loading="lazy"
+          />
+        </a>
+        <div class="card_title_container">
+          <h2 class="card_name">Chandan Kushwaha</h2>
+          <div className="card_details">
+            <p><i class="ri-mail-fill"></i>chandan@google.com</p>
+            <p><i class="ri-phone-fill"></i>+91-7050936557</p>
+            <p><i class="ri-global-fill"></i>https://chandanck22.vercel.app/</p>
+          </div>
+        </div>
+
+        <div class="card_footer_container">
+          <div class="card_tag_container">
+            <a href=""><i class="ri-service-fill"></i></a>
+          </div>
+          <div class="card_tag_container">
+            <a href="#"><i class="ri-edit-fill"></i></a>
+          </div>
+          <div class="card_tag_container">
+            <a href="#"><i class="ri-delete-bin-7-fill"></i></a>
+          </div>
+        </div>
+      </div>
+
+            {/* <!-- Card --> */}
+            <div class="card_container">
+        <a href="#" class="card_image_container">
+          <img
+            src="https://source.unsplash.com/random/600x400?girls"
+            alt="card image"
+            class="card_image"
+            loading="lazy"
+          />
+        </a>
+        <div class="card_title_container">
+          <h2 class="card_name">Chandan Kushwaha</h2>
+          <div className="card_details">
+            <p><i class="ri-mail-fill"></i>chandan@google.com</p>
+            <p><i class="ri-phone-fill"></i>+91-7050936557</p>
+            <p><i class="ri-global-fill"></i>https://chandanck22.vercel.app/</p>
+          </div>
+        </div>
+
+        <div class="card_footer_container">
+          <div class="card_tag_container">
+            <a href=""><i class="ri-service-fill"></i></a>
+          </div>
+          <div class="card_tag_container">
+            <a href="#"><i class="ri-edit-fill"></i></a>
+          </div>
+          <div class="card_tag_container">
+            <a href="#"><i class="ri-delete-bin-7-fill"></i></a>
+          </div>
+        </div>
+      </div>
+
+            {/* <!-- Card --> */}
+            <div class="card_container">
+        <a href="#" class="card_image_container">
+          <img
+            src="https://source.unsplash.com/random/600x400?girls"
+            alt="card image"
+            class="card_image"
+            loading="lazy"
+          />
+        </a>
+        <div class="card_title_container">
+          <h2 class="card_name">Chandan Kushwaha</h2>
+          <div className="card_details">
+            <p><i class="ri-mail-fill"></i>chandan@google.com</p>
+            <p><i class="ri-phone-fill"></i>+91-7050936557</p>
+            <p><i class="ri-global-fill"></i>https://chandanck22.vercel.app/</p>
+          </div>
+        </div>
+
+        <div class="card_footer_container">
+          <div class="card_tag_container">
+            <a href=""><i class="ri-service-fill"></i></a>
+          </div>
+          <div class="card_tag_container">
+            <a href="#"><i class="ri-edit-fill"></i></a>
+          </div>
+          <div class="card_tag_container">
+            <a href="#"><i class="ri-delete-bin-7-fill"></i></a>
+          </div>
+        </div>
+      </div>
+
+    </main>
+  )
+}
+
+export default Cards
